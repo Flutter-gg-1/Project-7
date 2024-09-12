de2d9c08755044fb5f947eaa4f7fb033aff2f796
muhannad <mohndalzaher@gmail.com>
2024-09-13 01:19:31 +0300

    finshed from first_screan

--- lib/screens/Auth/auth_screen.dart
+++ lib/screens/Auth/auth_screen.dart
@@ -5,6 +5,94 @@ class AuthScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xFF4129B7),
+                  Color(0xff1D1251),
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Image.asset(
+              'assets/Tuwaiq_mounten 1.png',
+              fit: BoxFit.cover,
+              height: 300,
+              width: double.infinity,
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/logo-h-white.png',
+                  height: 100,
+                ),
+                const SizedBox(height: 50),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 100, vertical: 9),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    backgroundColor: Color(0xffFFFFFF).withOpacity(0.72),
+                  ),
+                  child: const Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      color: Color(0xff4129B7),
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                RichText(
+                  text: TextSpan(
+                    text: "I'm already an account ",
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: 'Sign In',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          foreground: Paint()
+                            ..shader = const LinearGradient(
+                              colors: <Color>[
+                                Color(0xFFFFA17F),
+                                Color(0xFFFF8181),
+                              ],
+                            ).createShader(
+                              const Rect.fromLTWH(0.0, 0.0, 200.0, 70.0),
+                            ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+              top: 103, left: 350, child: Image.asset('assets/Group 10.png')),
+          Positioned(top: 620, child: Image.asset('assets/Group 10 (1).png')),
+        ],
+      ),
+    );
   }
 }
