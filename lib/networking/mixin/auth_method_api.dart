42403b03ce3489445e53910aaff38ec17a1b5abc
Ammarx9 <ammarxx919@gmail.com>
2024-09-13 02:01:22 +0300

    add all auth api

--- lib/networking/mixin/auth_method_api.dart
+++ lib/networking/mixin/auth_method_api.dart
@@ -1,3 +1,89 @@
+import 'package:dio/dio.dart';
 import 'package:tuwaiq_project/networking/networking_constant.dart';
 
-mixin AuthMethodApi on NetworkingConstant{}
+mixin AuthMethodApi on NetworkingConstant {
+  // checked
+  createUserPost(
+      {required String email,
+      required String fName,
+      required String lName}) async {
+
+
+
+        
+    try{
+
+       final res = await dio.post("$baseUrl$endCreateUser",
+        data: {"email": email, "f_name": fName, "l_name": lName});
+
+    print(res.data);
+
+    }  on DioException  catch(err){
+
+      print(err.response?.data["data"]);
+
+    }
+
+    
+    
+    
+    catch(err){
+
+      // print(err);
+
+    }
+   
+  }
+
+
+
+
+  loginPost({required String email}) async {
+
+
+
+
+    try{
+       final res = await dio.post("$baseUrl$endCreateUser",
+        data: {"email": email});
+
+    print(res.data);
+
+    }catch(err){
+
+    }
+
+
+   
+
+  }
+
+
+
+
+  verifyPost({required String email, required String otp}) async{
+
+
+    try{
+
+      final res = await dio.post("$baseUrl$endCreateUser",
+        data: {
+          
+          "email": email  ,
+
+          "otp" :otp
+        
+        
+        }  );
+
+    print(res.data);
+
+    }catch(err){
+
+    }
+
+
+    
+
+  }
+}
--- lib/networking/networking_api.dart
+++ lib/networking/networking_api.dart
@@ -1,3 +1,6 @@
-class NetworkingApi {
+import 'package:tuwaiq_project/networking/mixin/auth_method_api.dart';
+import 'package:tuwaiq_project/networking/networking_constant.dart';
+
+class NetworkingApi extends NetworkingConstant with AuthMethodApi {
   
 }
