e0f0384085184793834cfe4fc50588f311e4c074
rawan <rawan1993_7@hotmail.com>
2024-09-14 17:52:16 +0300

    add DevicePreview package and fix issus and make the design responsive for all devices

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:project_management_app/Bottom_Nav/bottom_nav.dart';
 import 'package:project_management_app/services/setup.dart';
@@ -6,7 +8,10 @@ import 'package:sizer/sizer.dart';
 void main() async {
   await setup();
 
-  runApp(const MainApp());
+  runApp(DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => const MainApp(), // Wrap your app
+  ),);
 }
 
 class MainApp extends StatelessWidget {
