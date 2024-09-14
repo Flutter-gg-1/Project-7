ca63489ad93b51632f2f6ac0788bbaa2f65e998d
Basel-75 <basel_1422@outlook.com>
2024-09-14 14:47:19 +0300

    Connect login & otp screens with api

--- lib/screens/auth/bloc/auth_bloc.dart
+++ lib/screens/auth/bloc/auth_bloc.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
+import 'package:tuwaiq_project/data_layer/auth_layer.dart';
 import 'package:tuwaiq_project/data_layer/language_layer.dart';
 import 'package:tuwaiq_project/networking/networking_api.dart';
 import 'package:tuwaiq_project/services/setup.dart';
@@ -17,6 +18,7 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
   TextEditingController? controllerLName = TextEditingController(text: "saad");
   AuthBloc() : super(AuthInitial()) {
     on<AuthEvent>((event, emit) {});
+
     //========Create Account============
     on<CreateAccountEvent>((event, emit) async {
       try {
@@ -34,6 +36,34 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
         emit(ErrorState(msg: "There is error with on the server"));
       }
     });
+    //=============Login=================
+    on<LoginEvent>((event, emit) async {
+      try {
+        emit(LoadingState());
+
+        final emailLogin = await api.userLogin(
+          email: controllerEmail!.text,
+        );
+        print(emailLogin);
+        emit(SuccessState());
+      } on FormatException catch (error) {
+        emit(ErrorState(msg: error.message));
+      } catch (error) {
+        emit(ErrorState(msg: "There is error with on the server"));
+      }
+    });
+    //=============otp verfication=================
+    on<VerfieyEvent>((event, emit) async {
+    try {
+      emit(LoadingState());
+      final userAuth = await api.verifyOtp(email: event.email, otp: event.otp);
+      await authLocator.get<AuthLayerData>().saveAuth(authData: userAuth);
+      emit(SuccessState());
+    } catch (error) {
+      emit(ErrorState(msg: "There is error with on the server"));
+    }
+    });
+
     //========Change Language============
     on<LanguageChangeEvent>((event, emit) {
       language.isArabic = !language.isArabic;
--- lib/screens/auth/otp_screen.dart
+++ lib/screens/auth/otp_screen.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pinput/pinput.dart';
+
 import 'package:tuwaiq_project/helper/extinsion/size_config.dart';
 import 'package:tuwaiq_project/screens/auth/bloc/auth_bloc.dart';
+import 'package:tuwaiq_project/screens/home_screen.dart';
 import 'package:tuwaiq_project/widget/button/custom_button.dart';
 
 class OtpScreen extends StatelessWidget {
-  const OtpScreen({super.key});
+  final String email;
+  const OtpScreen({
+    super.key,
+    required this.email,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -14,79 +20,106 @@ class OtpScreen extends StatelessWidget {
       create: (context) => AuthBloc(),
       child: Builder(builder: (context) {
         final bloc = context.read<AuthBloc>();
-        return Container(
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage('assets/image/auth_background.jpg'),
-                  fit: BoxFit.cover)),
-          child: Scaffold(
-            backgroundColor: Colors.transparent,
-            body: SingleChildScrollView(
-              child: Center(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(
-                      horizontal: context.getWidth(multiply: 0.045)),
-                  child: Column(
-                    children: [
-                      context.addSpacer(multiply: 0.08),
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: IconButton(
-                            style: ButtonStyle(
-                              padding: WidgetStateProperty.all(EdgeInsets.zero),
-                            ),
-                            onPressed: () => Navigator.pop(context),
-                            icon: const Icon(Icons.arrow_back_ios_new_rounded)),
-                      ),
-                      Image.asset('assets/image/otp_image.png'),
-                      Row(
-                        mainAxisAlignment: bloc.language.isArabic
-                            ? MainAxisAlignment.end
-                            : MainAxisAlignment.start,
-                        children: [
-                          Text(
-                            bloc.language.isArabic
-                                ? ':رمز التحقق'
-                                : 'OTP Verification: ',
-                            style: const TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.w600),
-                          ),
-                          const Text(
-                            '*',
-                            style: TextStyle(color: Colors.red, fontSize: 18),
-                          )
-                        ],
-                      ),
-                      context.addSpacer(multiply: 0.02),
-                      const Pinput(
-                        length: 6,
-                      ),
-                      context.addSpacer(multiply: 0.01),
-                      Align(
-                          alignment: Alignment.centerRight,
-                          child: TextButton(
-                            style: ButtonStyle(
-                              padding: WidgetStateProperty.all(EdgeInsets.zero),
-                            ),
-                            onPressed: () {},
-                            child: Text(
+        return BlocListener<AuthBloc, AuthState>(
+          listener: (context, state) {
+            if (state is LoadingState) {
+              showDialog(
+                  barrierDismissible: false,
+                  context: context,
+                  builder: (context) =>
+                      const Center(child: CircularProgressIndicator()));
+            }
+            if (state is SuccessState) {
+              Navigator.pop(context);
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) => const HomeScreen()));
+            }
+            if (state is ErrorState) {
+              Navigator.pop(context);
+              ScaffoldMessenger.of(context)
+                  .showSnackBar( SnackBar(content: Text(state.msg)));
+            }
+          },
+          child: Container(
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/image/auth_background.jpg'),
+                    fit: BoxFit.cover)),
+            child: Scaffold(
+              backgroundColor: Colors.transparent,
+              body: SingleChildScrollView(
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: context.getWidth(multiply: 0.045)),
+                    child: Column(
+                      children: [
+                        context.addSpacer(multiply: 0.08),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: IconButton(
+                              style: ButtonStyle(
+                                padding:
+                                    WidgetStateProperty.all(EdgeInsets.zero),
+                              ),
+                              onPressed: () => Navigator.pop(context),
+                              icon:
+                                  const Icon(Icons.arrow_back_ios_new_rounded)),
+                        ),
+                        Image.asset('assets/image/otp_image.png'),
+                        Row(
+                          mainAxisAlignment: bloc.language.isArabic
+                              ? MainAxisAlignment.end
+                              : MainAxisAlignment.start,
+                          children: [
+                            Text(
                               bloc.language.isArabic
-                                  ? 'اعادة ارسال رمز التحقق'
-                                  : 'resend OTP',
+                                  ? ':رمز التحقق'
+                                  : 'OTP Verification: ',
                               style: const TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w600,
-                                  color: Color(0xff57E3D8)),
+                                  fontSize: 20, fontWeight: FontWeight.w600),
                             ),
-                          )),
-                      context.addSpacer(multiply: 0.06),
-                      CustomButton(
-                        englishTitle: 'Verify',
-                        arabicTitle: 'تحقق',
-                        onPressed: () {},
-                        arabic: bloc.language.isArabic,
-                      )
-                    ],
+                            const Text(
+                              '*',
+                              style: TextStyle(color: Colors.red, fontSize: 18),
+                            )
+                          ],
+                        ),
+                        context.addSpacer(multiply: 0.02),
+                         Pinput(
+                          length: 6,
+                          onCompleted: (value) => bloc.otp = value,
+                        ),
+                        context.addSpacer(multiply: 0.01),
+                        Align(
+                            alignment: Alignment.centerRight,
+                            child: TextButton(
+                              style: ButtonStyle(
+                                padding:
+                                    WidgetStateProperty.all(EdgeInsets.zero),
+                              ),
+                              onPressed: () {},
+                              child: Text(
+                                bloc.language.isArabic
+                                    ? 'اعادة ارسال رمز التحقق'
+                                    : 'resend OTP',
+                                style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w600,
+                                    color: Color(0xff57E3D8)),
+                              ),
+                            )),
+                        context.addSpacer(multiply: 0.06),
+                        CustomButton(
+                          englishTitle: 'Verify',
+                          arabicTitle: 'تحقق',
+                          onPressed: () {
+                            bloc.add(VerfieyEvent(email: email));
+                          },
+                          arabic: bloc.language.isArabic,
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
