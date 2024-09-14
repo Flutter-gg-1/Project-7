7f188ef1821adf05b03f925e23914cd891f98a4f
Basel-75 <basel_1422@outlook.com>
2024-09-14 13:50:37 +0300

    :Create auth data layer

--- lib/networking/mixin/auth_method_api.dart
+++ lib/networking/mixin/auth_method_api.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:tuwaiq_project/models/user_model.dart';
 import 'package:tuwaiq_project/networking/networking_constant.dart';
 
 mixin AuthMethodApi on NetworkingConstant {
@@ -7,83 +8,44 @@ mixin AuthMethodApi on NetworkingConstant {
       {required String email,
       required String fName,
       required String lName}) async {
-
-
-
-        
-    try{
-
-       final res = await dio.post("$baseUrl$endCreateUser",
-        data: {"email": email, "f_name": fName, "l_name": lName});
-
-    print(res.data);
-
-    }  on DioException  catch(err){
-
-      print(err.response?.data["data"]);
-
+    try {
+      final res = await dio.post("$baseUrl$endCreateUser", data: {
+        "email": email.trim(),
+        "f_name": fName.trim(),
+        "l_name": lName.trim()
+      });
+    } on DioException catch (error) {
+      throw FormatException(error.response?.data["data"]);
+    } catch (error) {
+      throw const FormatException("~there error with Server");
     }
-
-    
-    
-    
-    catch(err){
-
-      // print(err);
-
-    }
-   
   }
-
-
-
 
   loginPost({required String email}) async {
+    try {
+      final res =
+          await dio.post("$baseUrl$endCreateUser", data: {"email": email});
 
-
-
-
-    try{
-       final res = await dio.post("$baseUrl$endCreateUser",
-        data: {"email": email});
-
-    print(res.data);
-
-    }catch(err){
-
+      print(res.data);
+    } on DioException catch (error) {
+      throw FormatException(error.response?.data["data"]);
+    } catch (error) {
+      throw const FormatException("~there error with Server");
     }
-
-
-   
-
   }
 
-
-
 // form here take token
-  verifyPost({required String email, required String otp}) async{
-
-
-    try{
-
-      final res = await dio.post("$baseUrl$endCreateUser",
-        data: {
-          
-          "email": email  ,
-
-          "otp" :otp
-        
-        
-        }  );
-
-    print(res.data);
-
-    }catch(err){
-
+  verifyPost({required String email, required String otp}) async {
+    try {
+      final res = await dio
+          .post("$baseUrl$endCreateUser", data: {"email": email, "otp": otp});
+      
+      print(res.data);
+      return UserModel.fromJson(res.data["data"]);
+    } on DioException catch (error) {
+      throw FormatException(error.response?.data["data"]);
+    } catch (error) {
+      throw const FormatException("~there error with Server");
     }
-
-
-    
-
   }
 }
--- lib/services/setup.dart
+++ lib/services/setup.dart
@@ -1,10 +1,13 @@
 import 'package:get_it/get_it.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:tuwaiq_project/data_layer/auth_layer.dart';
 import 'package:tuwaiq_project/data_layer/language_layer.dart';
 
 final languageLocaitor = GetIt.instance;
+final authLocator = GetIt.instance;
 
 Future<void> setup() async{
   await GetStorage.init();
   languageLocaitor.registerSingleton<LanguageLayer>(LanguageLayer());
+  authLocator.registerSingleton<AuthLayerData>(AuthLayerData());
 }
