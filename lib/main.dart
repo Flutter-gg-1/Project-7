151d9802f70ae7d3c4b9bece952fb22cf0cb0c05
rawan <rawan1993_7@hotmail.com>
2024-09-14 16:30:45 +0300

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:project_management_app/Bottom_Nav/bottom_nav.dart';
-import 'package:project_management_app/screens/Auth/auth_screen.dart';
-import 'package:project_management_app/screens/Home/home_screen.dart';
+import 'package:project_management_app/services/setup.dart';
+import 'package:sizer/sizer.dart';
+
+void main() async {
+  await setup();
+
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Sizer(
+      builder: (context, orientation, deviceType) {
+        return const MaterialApp(
+          home: BottomNav(),
+        );
+      },
+    );
+  }
+}
+
+import 'package:flutter/material.dart';
+import 'package:project_management_app/Bottom_Nav/bottom_nav.dart';
+import 'package:project_management_app/screens/Project/evaluation_screen.dart';
 import 'package:project_management_app/services/setup.dart';
 import 'package:sizer/sizer.dart';
 
