0e29e5d252ea424c59e275353e26387c2952b88c
rawan <rawan1993_7@hotmail.com>
2024-09-14 16:31:25 +0300

    change

--- lib/main.dart
+++ lib/main.dart
@@ -23,30 +23,3 @@ class MainApp extends StatelessWidget {
     );
   }
 }
-
-import 'package:flutter/material.dart';
-import 'package:project_management_app/Bottom_Nav/bottom_nav.dart';
-import 'package:project_management_app/screens/Project/evaluation_screen.dart';
-import 'package:project_management_app/services/setup.dart';
-import 'package:sizer/sizer.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await setup();
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Sizer(
-      builder: (context, orientation, deviceType) {
-        return const MaterialApp(
-          home: EvaluationScreen(),
-        );
-      },
-    );
-  }
-}
