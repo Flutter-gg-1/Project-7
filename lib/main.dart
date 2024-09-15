ff67af3c8154d4d18717d889ab2dfa40178ac759
Abdulaziz_Alamri <aazizalamri5@gmail.com>
2024-09-15 23:04:30 +0300

    pulled

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() async {
 
   runApp(
     const MainApp()
-
+ 
     // DevicePreview(
     //   enabled: !kReleaseMode,
     //   builder: (context) => const MainApp(), // Wrap your app
