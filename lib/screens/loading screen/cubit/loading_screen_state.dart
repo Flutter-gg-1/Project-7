574277f23ade2d35a7037d61f6dc8ecf9a75e2a2
Amjad Elahi <amjadelahi07@gmail.com>
2024-09-15 22:50:49 +0300

    update text custom widget

--- lib/screens/loading screen/cubit/loading_screen_state.dart
+++
@@ -1,9 +0,0 @@
-part of 'loading_screen_cubit.dart';
-
-@immutable
-sealed class LoadingScreenState {}
-
-final class LoadingScreenInitial extends LoadingScreenState {}
-final class LoadingState extends LoadingScreenState {}
-final class LoggedInState extends LoadingScreenState {}
-final class NotLoggedInState extends LoadingScreenState {}
