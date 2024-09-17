95f80b0f7acc1311dfbc5d491a94b0a8cf48c32e
Abdulaziz_Alamri <aazizalamri5@gmail.com>
2024-09-17 22:10:20 +0300

    fixed mistake

--- lib/screens/Edit_Project/edit_project.dart
+++ lib/screens/Edit_Project/edit_project.dart
@@ -82,130 +82,126 @@ class EditProjectScreen extends StatelessWidget {
             ),
             const SizedBox(height: 20),
 
-                  // Upload Presentation Section
-                  Column(
-                    children: [
-                      const Text(
-                        "Upload Presentation",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
-                          color: Color(0xff4129B7),
+            // Upload Presentation Section
+            Column(
+              children: [
+                const Text(
+                  "Upload Presentation",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                    color: Color(0xff4129B7),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                DottedBorder(
+                  color: const Color(0xffD9D9D9),
+                  strokeWidth: 1,
+                  child: Container(
+                      height: 76,
+                      width: 150,
+                      decoration:
+                          BoxDecoration(borderRadius: BorderRadius.circular(3)),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.add,
+                          color: Colors.black,
                         ),
-                      ),
-                      const SizedBox(height: 10),
-                      DottedBorder(
-                        color: const Color(0xffD9D9D9),
-                        strokeWidth: 1,
-                        child: Container(
-                            height: 76,
-                            width: 150,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(3)),
-                            child: IconButton(
-                              onPressed: () {},
-                              icon: const Icon(
-                                Icons.add,
-                                color: Colors.black,
-                              ),
-                            )),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              const Divider(),
-              const SizedBox(height: 20),
-
-              // Text Fields Section
-              _buildTextField("Project Name", projectNameController),
-              const SizedBox(height: 16),
-              _buildTextField("Bootcamp Name", bootcampNameController),
-              const SizedBox(height: 16),
-              _buildTextField("Type", projectTypeController),
-              const SizedBox(height: 16),
-              _buildTextField("Start Date", startDateController, isDate: true),
-              const SizedBox(height: 16),
-              _buildTextField("End Date", endDateController, isDate: true),
-              const SizedBox(height: 16),
-              _buildTextField("Presentation Date", presentationDateController,
-                  isDate: true),
-              const SizedBox(height: 16),
-              _buildTextField(
-                  "Project Description", projectDescriptionController,
-                  isMultiline: true),
-
-              const SizedBox(height: 20),
-              const Divider(),
-              const SizedBox(height: 20),
-
-              // Links Section
-              const Text(
-                "Links: ",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xff4129B7), // لون النص
-                  fontSize: 18,
+                      )),
                 ),
-              ),
-              const SizedBox(height: 10),
-              _buildTextField("GitHub", githubController),
-              const SizedBox(height: 10),
-              _buildTextField("Play Store", playStoreController),
-              const SizedBox(height: 10),
-              _buildTextField("App Store", appleStoreController),
-              const SizedBox(height: 10),
-              _buildTextField("Figma", figmaController),
-              const SizedBox(height: 10),
-              _buildTextField("Web Link", webLinkController),
-              const SizedBox(height: 10),
-              _buildTextField("Pinterest", pinterestController),
+              ],
+            ),
+       
+        const SizedBox(height: 20),
+        const Divider(),
+        const SizedBox(height: 20),
 
-              const SizedBox(height: 20),
-              const Divider(),
-              const SizedBox(height: 20),
+        // Text Fields Section
+        _buildTextField("Project Name", projectNameController),
+        const SizedBox(height: 16),
+        _buildTextField("Bootcamp Name", bootcampNameController),
+        const SizedBox(height: 16),
+        _buildTextField("Type", projectTypeController),
+        const SizedBox(height: 16),
+        _buildTextField("Start Date", startDateController, isDate: true),
+        const SizedBox(height: 16),
+        _buildTextField("End Date", endDateController, isDate: true),
+        const SizedBox(height: 16),
+        _buildTextField("Presentation Date", presentationDateController,
+            isDate: true),
+        const SizedBox(height: 16),
+        _buildTextField("Project Description", projectDescriptionController,
+            isMultiline: true),
 
-              // Members Section
-              const Text(
-                "Members",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                  color: Color(0xff4129B7), // لون النص
-                ),
-              ),
-              const SizedBox(height: 10),
-              _buildTextField("Member 1", member1Controller),
-              const SizedBox(height: 10),
-              _buildTextField("Member 2", member2Controller),
-              const SizedBox(height: 10),
-              _buildTextField("Member 3", member3Controller),
-              const SizedBox(height: 20),
+        const SizedBox(height: 20),
+        const Divider(),
+        const SizedBox(height: 20),
 
-              // Save Button
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Action to save the project details
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xff4129B7),
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 40, vertical: 15),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  child: const Text(
-                    "Save",
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 70),
-            ],
+        // Links Section
+        const Text(
+          "Links: ",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Color(0xff4129B7), // لون النص
+            fontSize: 18,
           ),
+        ),
+        const SizedBox(height: 10),
+        _buildTextField("GitHub", githubController),
+        const SizedBox(height: 10),
+        _buildTextField("Play Store", playStoreController),
+        const SizedBox(height: 10),
+        _buildTextField("App Store", appleStoreController),
+        const SizedBox(height: 10),
+        _buildTextField("Figma", figmaController),
+        const SizedBox(height: 10),
+        _buildTextField("Web Link", webLinkController),
+        const SizedBox(height: 10),
+        _buildTextField("Pinterest", pinterestController),
+
+        const SizedBox(height: 20),
+        const Divider(),
+        const SizedBox(height: 20),
+
+        // Members Section
+        const Text(
+          "Members",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+            color: Color(0xff4129B7), // لون النص
+          ),
+        ),
+        const SizedBox(height: 10),
+        _buildTextField("Member 1", member1Controller),
+        const SizedBox(height: 10),
+        _buildTextField("Member 2", member2Controller),
+        const SizedBox(height: 10),
+        _buildTextField("Member 3", member3Controller),
+        const SizedBox(height: 20),
+
+        // Save Button
+        Center(
+          child: ElevatedButton(
+            onPressed: () {
+              // Action to save the project details
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color(0xff4129B7),
+              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            child: const Text(
+              "Save",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        const SizedBox(height: 70),
+           ],
         ),
       ),
     );
