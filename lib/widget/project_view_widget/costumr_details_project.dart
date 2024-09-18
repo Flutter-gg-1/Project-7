0a16ee030e59525e699f99a5e3a7e0395dfdc634
Anas Aljuhani <Anas.aljuhani.dev@gmail.com>
2024-09-18 22:02:46 +0300

    project view screen ui done

---
+++ lib/widget/project_view_widget/costumr_details_project.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:tuwaiq_project/helper/extinsion/size_config.dart';
+
+class CostomeDetailsProject extends StatelessWidget {
+  const CostomeDetailsProject({
+    super.key,
+    required this.cotentText,
+    required this.heightContainer,
+    required this.widthContainer,
+  });
+
+  final String cotentText;
+  final double heightContainer;
+  final double widthContainer;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        vertical: context.getHeight(multiply: 0.01),
+        horizontal: context.getHeight(multiply: 0.04),
+      ),
+      width: widthContainer,
+      height: heightContainer,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+        boxShadow: const [
+          BoxShadow(
+            color: Color.fromARGB(255, 196, 196, 196),
+            spreadRadius: 1,
+            blurRadius: 2,
+            offset: Offset(0.5, 4),
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const Text(
+            'details project',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
+          ),
+          ConstrainedBox(
+            constraints: BoxConstraints(
+              maxHeight: context.getHeight(multiply: 0.2),
+            ),
+            child: Scrollbar(
+              child: SingleChildScrollView(
+                child: TextFormField(
+                  initialValue: cotentText,
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                  ),
+                  keyboardType: TextInputType.multiline,
+                  maxLines: null,
+                  minLines: 1,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
