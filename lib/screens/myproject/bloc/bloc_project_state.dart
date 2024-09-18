fc084d0a7d3755651632d46c1632f7a21d274bcc
dev <nooof-2017a@hotmail.com>
2024-09-19 01:28:02 +0300

    editing for bloc reating and myproject

---
+++ lib/screens/myproject/bloc/bloc_project_state.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+import 'package:project_judge/models/project_model.dart';
+
+abstract class ProjectState {}
+
+class ProjectLoading extends ProjectState {}
+
+class ProjectLoaded extends ProjectState {
+  final List<Project> projects;
+
+  ProjectLoaded(this.projects);
+}
+
+class ProjectError extends ProjectState {
+  final String message;
+
+
+  ProjectError(this.message);
+}
