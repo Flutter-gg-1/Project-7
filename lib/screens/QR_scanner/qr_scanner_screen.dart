3580455f2da39a8198934f05d311f36f0ae8087d
rawan <rawan1993_7@hotmail.com>
2024-09-19 19:23:20 +0300

    add bloc for qr code screen

--- lib/screens/QR_scanner/qr_scanner_screen.dart
+++ lib/screens/QR_scanner/qr_scanner_screen.dart
@@ -1,65 +1,68 @@
+// qr_scanner_screen.dart
+
 import 'package:flutter/material.dart';
-import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:project_management_app/screens/QR_scanner/bloc/barcode_scanner_bloc.dart';
+import 'package:project_management_app/screens/QR_scanner/bloc/barcode_scanner_event.dart';
+import 'package:project_management_app/screens/QR_scanner/bloc/barcode_scanner_state.dart';
+
 import 'package:project_management_app/theme/appcolors.dart';
 
-class QrScannerScreen extends StatefulWidget {
+class QrScannerScreen extends StatelessWidget {
   const QrScannerScreen({super.key});
-
-  @override
-  _QrScannerScreenState createState() => _QrScannerScreenState();
-}
-
-class _QrScannerScreenState extends State<QrScannerScreen> {
-  String barcodeScanRes = 'Unknown';
-
-  // Function to start scanning
-  Future<void> startBarcodeScan() async {
-    String scanResult;
-    try {
-      scanResult = await FlutterBarcodeScanner.scanBarcode(
-        '#FF6666', // Color overlay for the scanner window
-        'Cancel', // Cancel button text
-        true, // Show flash icon for better lighting in low light conditions
-        ScanMode
-            .BARCODE, // You can set ScanMode.QR for QR scanning or BARCODE for barcodes
-      );
-      if (!mounted) return;
-      setState(() {
-        barcodeScanRes = scanResult != '-1' ? scanResult : 'Scan canceled';
-      });
-    } catch (e) {
-      setState(() {
-        barcodeScanRes = 'Failed to scan the barcode.';
-      });
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('QR Code Scanner',
-            style: TextStyle(color: AppColors.white)),
+        title: const Text('QR Code Scanner', style: TextStyle(color: AppColors.white)),
         backgroundColor: AppColors.blueDark,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: startBarcodeScan,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: AppColors
-                    .blueDark, // Set the background color to AppColors.blueDark
-              ),
-              child: const Text(
-                'Start Barcode Scan',
-                style:
-                    TextStyle(color: Colors.white), // Text color set to white
-              ),
+      body: BlocProvider(
+        create: (_) => BarcodeScannerBloc(),
+        child: const BarcodeScannerView(),
+      ),
+    );
+  }
+}
+
+class BarcodeScannerView extends StatelessWidget {
+  const BarcodeScannerView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BlocBuilder<BarcodeScannerBloc, BarcodeScannerState>(
+            builder: (context, state) {
+              if (state is BarcodeInitial) {
+                return const Text('Press the button to start scanning.');
+              } else if (state is BarcodeScanning) {
+                return const CircularProgressIndicator(); // Show progress during scan
+              } else if (state is BarcodeScanned) {
+                return Text('Scan result: ${state.result}'); // Display result after scan
+              } else if (state is BarcodeScanFailed) {
+                return const Text('Failed to scan the barcode.'); // Show error message
+              }
+              return const SizedBox.shrink();
+            },
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              BlocProvider.of<BarcodeScannerBloc>(context).add(StartScan()); // Trigger scan event
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: AppColors.blueDark, // Set button background color
             ),
-          ],
-        ),
+            child: const Text(
+              'Start Barcode Scan',
+              style: TextStyle(color: Colors.white), // Set text color to white
+            ),
+          ),
+        ],
       ),
     );
   }
