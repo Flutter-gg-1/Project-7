1e95fc4e05f1a2f3bfb5bade3df5a6ab41d8a535
Amjad Elahi <amjadelahi07@gmail.com>
2024-09-20 14:25:41 +0300

    update verify screen

--- lib/screens/auth/verify_screen.dart
+++ lib/screens/auth/verify_screen.dart
@@ -59,11 +59,109 @@ class VerifyScreen extends StatelessWidget {
                         constraints: BoxConstraints(
                             maxHeight:
                                 MediaQuery.of(context).size.height - 136),
+                        width: double.infinity,
                         decoration: const BoxDecoration(
                             color: Color(0x65000000),
                             borderRadius: BorderRadius.only(
                                 topRight: Radius.circular(40),
                                 topLeft: Radius.circular(40))),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 100),
+                          child: Column(
+                            children: [
+                              const CustomText(
+                                  text: "Verification code",
+                                  size: 32,
+                                  color: Colors.white),
+                              const SizedBox(height: 50),
+                              SizedBox(
+                                width: 318,
+                                height: 48,
+                                child: RichText(
+                                  text: TextSpan(children: [
+                                    const TextSpan(
+                                      text:
+                                          "Please enter the code that has been sent to the email ",
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                    TextSpan(
+                                      text: email,
+                                      style: const TextStyle(
+                                        fontSize: 16,
+                                        color: Color(0xff57E3D8),
+                                      ),
+                                    )
+                                  ]),
+                                ),
+                              ),
+                              const SizedBox(height: 122),
+                              Pinput(
+                                  cursor: Column(
+                                    children: [
+                                      Container(
+                                        width: 49,
+                                        height: 4611,
+                                        decoration: BoxDecoration(
+                                            color: Colors.white,
+                                            borderRadius:
+                                                BorderRadius.circular(8),
+                                            border: Border.all(
+                                                color: const Color(0xff4D2EB4),
+                                                width: 2)),
+                                      )
+                                    ],
+                                  ),
+                                  pinAnimationType: PinAnimationType.scale,
+                                  defaultPinTheme: PinTheme(
+                                      width: 49,
+                                      height: 48,
+                                      textStyle: const TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w500,
+                                          fontSize: 16),
+                                      decoration: BoxDecoration(
+                                          color: Colors.white,
+                                          borderRadius:
+                                              BorderRadius.circular(8))),
+                                  focusedPinTheme: PinTheme(
+                                      width: 49,
+                                      height: 48,
+                                      textStyle: const TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w500,
+                                          fontSize: 16),
+                                      decoration: BoxDecoration(
+                                          border: Border.all(
+                                              color: const Color(0xff4D2EB4)),
+                                          borderRadius:
+                                              BorderRadius.circular(8),
+                                          color: Colors.white)),
+                                  controller: cubit.otpController,
+                                  length: 6,
+                                  inputFormatters: [
+                                    FilteringTextInputFormatter.digitsOnly
+                                  ],
+                                  onCompleted: (pin) {
+                                    cubit.checkAuth(email: email);
+                                  }),
+                              const SizedBox(height: 10),
+                              TextButton(
+                                  onPressed: () {
+                                    cubit.otpController.clear();
+                                    cubit.resendOTP(email: email);
+                                  },
+                                  child: const CustomText(
+                                    text: "Resend OTP",
+                                    size: 14,
+                                    color: Colors.white,
+                                    weight: FontWeight.w700,
+                                  ))
+                            ],
+                          ),
+                        ),
                       ),
                       Positioned(
                           top: -50,
@@ -78,69 +176,6 @@ class VerifyScreen extends StatelessWidget {
                               child: Image.asset("assets/images/logo-h 2.png"),
                             ),
                           )),
-                      const Positioned(
-                        top: 90,
-                        left: 80,
-                        child: Text(
-                          "Verification code",
-                          style: TextStyle(color: Colors.white, fontSize: 32),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        width: 318,
-                        height: 48,
-                        child: RichText(
-                          text: TextSpan(children: [
-                            const TextSpan(
-                              text:
-                                  "Please enter the code that has been sent to the email ",
-                              style: TextStyle(
-                                color: Colors.white,
-                              ),
-                            ),
-                            TextSpan(
-                              text: email,
-                              style: const TextStyle(
-                                color: Color(0xff57E3D8),
-                              ),
-                            )
-                          ]),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 130,
-                      ),
-                      Pinput(
-                          controller: cubit.otpController,
-                          length: 6,
-                          inputFormatters: [
-                            FilteringTextInputFormatter.digitsOnly
-                          ],
-                          onCompleted: (pin) {
-                            cubit.checkAuth(email: email);
-                          }),
-                      const SizedBox(
-                        height: 16,
-                      ),
-                      TextButton(
-                          onPressed: () {
-                            cubit.otpController.clear();
-
-                            cubit.resendOTP(email: email);
-                          },
-                          child: const CustomText(
-                            text: "Resend OTP",
-                            size: 24,
-                            color: Colors.white,
-                            weight: FontWeight.w700,
-                          ))
                     ],
                   ),
                 ),
