14df3f13a0efb007074ee4cace835be500874f14
Basel-75 <basel_1422@outlook.com>
2024-09-22 12:56:35 +0300

    Remove dublicated user project screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,8 +11,9 @@ import 'package:tuwaiq_project/screens/all_project/all_project_screen.dart';
 import 'package:tuwaiq_project/screens/bottomNavBloc/bottomnav_bloc.dart';
 import 'package:tuwaiq_project/screens/profile/profile_screen.dart';
 import 'package:tuwaiq_project/screens/rating/rating_screen.dart';
-import 'package:tuwaiq_project/screens/search_screen.dart';
-import 'package:tuwaiq_project/screens/user_project_screen.dart';
+import 'package:tuwaiq_project/screens/serch_screen/search_screen.dart';
+import 'package:tuwaiq_project/screens/user_project/user_project_screen.dart';
+
 import 'package:tuwaiq_project/shape/auth_shape.dart';
 
 class HomeScreen extends StatelessWidget {
