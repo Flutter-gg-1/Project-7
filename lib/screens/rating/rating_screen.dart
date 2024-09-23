dcaf4da7f767fc593c7b9cb67e5ad4e59bd18af5
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-09-23 11:13:03 +0300

    UPD img

---
+++ lib/screens/rating/rating_screen.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+import 'package:image/image.dart' as img;
+import 'package:zxing2/qrcode.dart';
+
+readQR() async {
+  var image = img.decodePng(File('tool/example.png').readAsBytesSync())!;
+
+  LuminanceSource source = RGBLuminanceSource(
+      image.width,
+      image.height,
+      image
+          .convert(numChannels: 4)
+          .getBytes(order: img.ChannelOrder.rgba)
+          .buffer
+          .asInt32List());
+  var bitmap = BinaryBitmap(HybridBinarizer(source));
+
+  var reader = QRCodeReader();
+  var result = reader.decode(bitmap);
+  print(result.text);
+}
